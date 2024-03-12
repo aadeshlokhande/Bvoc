4f2ac2ffda12a486e750ae01a751d70809840af8
Aadesh <aadeshlokhande11@gmail.com>
2024-03-12 19:43:26 +0530

    12 / 03 / 2024

--- 6 SixSem/Internship/001.py
+++
@@ -1,8 +0,0 @@
-from phoneinfopy import register_phone_number
-
-number = "+917058232826"
-response = register_phone_number(number)
-print(response)
-
-
-{'status': True, 'message': 'Sent an OTP for +917058232826', 'requestId': '56b873f3-9fa9-41e9-9f2e-9552236cf36b'}
