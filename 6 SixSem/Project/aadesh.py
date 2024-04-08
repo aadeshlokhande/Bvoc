ac1d2a63cefef77886d49addecaf053d23764672
Aadesh <aadeshlokhande11@gmail.com>
2024-04-08 14:49:14 +0530

    08 / 04 / 2024

---
+++ 6 SixSem/Project/aadesh.py
@@ -0,0 +1,21 @@
+
+a = ['subscription.html',
+ 'event-list.html',
+ 'index.html',
+ 'privacy-policy.html',
+ 'course-filter-one-toggle.html', 
+ 'single-product.html',
+ 'instructor.html',
+ 'contact.html',
+ 'blog.html',
+ 'event-details.html',
+ 'blog-details.html',
+ '404.html']
+
+a.sort()
+
+# print(a)
+for i in a:
+    # url = "http://127.0.0.1:5501/"+i 
+    # print(url)
+    print(i)
