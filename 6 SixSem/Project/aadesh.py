303bdc103a933d25f1f3ad54f18e197e9c425d45
Aadesh <aadeshlokhande11@gmail.com>
2024-04-09 18:07:00 +0530

    09 / 04 / 2024

--- 6 SixSem/Project/aadesh.py
+++ 6 SixSem/Project/aadesh.py
@@ -1,21 +1,32 @@
 
-a = ['subscription.html',
- 'event-list.html',
- 'index.html',
- 'privacy-policy.html',
- 'course-filter-one-toggle.html', 
- 'single-product.html',
- 'instructor.html',
- 'contact.html',
- 'blog.html',
- 'event-details.html',
- 'blog-details.html',
- '404.html']
+# a = ['subscription.html',
+#  'event-list.html',
+#  'index.html',
+#  'privacy-policy.html',
+#  'course-filter-one-toggle.html', 
+#  'single-product.html',
+#  'instructor.html',
+#  'contact.html',
+#  'blog.html',
+#  'event-details.html',
+#  'blog-details.html',
+#  '404.html']
 
-a.sort()
+# a.sort()
 
+# # print(a)
+# for i in a:
+#     # url = "http://127.0.0.1:5501/"+i 
+#     # print(url)
+#     print(i)
+
+
+# a = "Hello! I'm interested in joining your programming courses at ICE Computers. Can you please provide me with more details? Thank you!".replace(" ", "%20")
+
+
+# url = f"wa.me/7058232826/?text={a}"
+# print(url)
+
+# a = "aadeshlokhande11@gmail.com".upper()
 # print(a)
-for i in a:
-    # url = "http://127.0.0.1:5501/"+i 
-    # print(url)
-    print(i)
+
