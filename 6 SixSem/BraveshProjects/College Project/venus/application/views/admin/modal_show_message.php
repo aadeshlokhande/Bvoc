6312b28734390db1f217390d9b38d238603214eb
Aadesh <aadeshlokhande11@gmail.com>
2024-04-21 19:42:23 +0530

    21 / 04 / 2024

--- 6 SixSem/BraveshProjects/College Project/venus/application/views/admin/modal_show_message.php
+++
@@ -1,3 +0,0 @@
-<p style="font-size: 13px">
-	<?php echo $this->security->xss_clean($this->db->get_where('contact_us_message', array('contact_us_message_id' => $param2) )->row()->message); ?>
-</p>
--- 6 SixSem/BraveshProjects/College Project/venus/application/views/alumni/modal_show_terms_and_conditions.php
+++
@@ -1,1 +0,0 @@
-<?php echo $this->db->get_where('about_us', array('about_us_id' => 1))->row()->terms_and_conditions; ?>
