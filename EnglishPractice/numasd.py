588d8aa923999e08d41b62b41e17f2b6578b10cb
Aadesh <aadeshlokhande11@gmail.com>
2025-02-21 21:37:55 +0530

    none

--- EnglishPractice/numasd.py
+++ EnglishPractice/numasd.py
@@ -1,15 +1,373 @@
-# a = [12,43,65]
-# b = a 
-# a.append(b)
+1. What is the correct way to start a Python comment?
+A) /* Comment */
+B) # Comment
+C) <!-- Comment -->
+D) // Comment
+Answer: B
 
-# print(a[3])
-# print(a[3][3])
-# print(a[3][3][3])
-# print(a[3][3][3][3])
-# print(a[3][3][3][3][3])
-# print(a[3][3][3][3][3][3])
-# print(a[3][3][3][3][3][3][3])
-# print(a[3][3][3][3][3][3][3][3])
-# print(a[3][3][3][3][3][3][3][3][3])
-# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+2. Which function is used to get user input in Python?
+A) input()
+B) read()
+C) get()
+D) scanf()
+Answer: A
 
+3. What will print(2 ** 3) output?
+A) 6
+B) 8
+C) 9
+D) 16
+Answer: B
+
+4. Which of the following is a valid variable name in Python?
+A) 1name
+B) my-name
+C) _name
+D) class
+Answer: C
+
+5. What is the output of print(10 // 3)?
+A) 3.33
+B) 3
+C) 4
+D) 3.0
+Answer: B
+
+
+6. What data type is the result of type(10.5)?
+A) int
+B) str
+C) float
+D) bool
+Answer: C
+
+7. Which type is mutable in Python?
+A) Tuple
+B) List
+C) String
+D) Integer
+Answer: B
+
+8. What is the output of print(bool(0))?
+A) True
+B) False
+C) None
+D) 0
+Answer: B
+
+9. How do you convert a string "123" into an integer?
+A) str(123)
+B) int("123")
+C) float(123)
+D) char(123)
+Answer: B
+
+10. What will print(len("Python")) output?
+A) 5
+B) 6
+C) 7
+D) Error
+Answer: B
+
+
+11. Which statement is used to exit a loop immediately?
+A) exit
+B) continue
+C) break
+D) stop
+Answer: C
+
+12. What will if 0: evaluate to?
+A) True
+B) False
+C) None
+D) Error
+Answer: B
+
+13. What is the output of:
+python
+x = 5
+while x > 0:
+x -= 1
+print(x)
+
+A) 5
+B) 0
+C) 1
+D) -1
+Answer: B
+
+14. How many times will the following for loop execute?
+python
+for i in range(2, 10, 2):
+print(i)
+
+A) 3
+B) 4
+C) 5
+D) 8
+Answer: B
+
+15. Which keyword is used to skip the current iteration and move to the next in a loop?
+A) pass
+B) break
+C) continue
+D) return
+Answer: C
+
+
+16. What keyword is used to define a function in Python?
+A) function
+B) def
+C) define
+D) func
+Answer: B
+
+17. What is the output of:
+python
+def example():
+return 10
+print(example() + 5)
+
+A) 15
+B) 10
+C) 5
+D) None
+Answer: A
+
+18. What is the default return value of a function if no return statement is used?
+A) 0
+B) False
+C) None
+D) Empty string
+Answer: C
+
+19. How do you call a function my_func with arguments 10 and 20?
+A) my_func[10, 20]
+B) my_func{10, 20}
+C) my_func(10, 20)
+D) my_func<10, 20>
+Answer: C
+
+20. Which statement is true about Python functions?
+A) They can return multiple values.
+B) They cannot have default arguments.
+C) They must return a value.
+D) They cannot be nested.
+Answer: A
+
+
+21. What is the output of "Hello"[1:4]?
+A) ell
+B) Hel
+C) elllo
+D) ello
+Answer: A
+
+22. Which method is used to convert a string to lowercase?
+A) str.lower()
+B) string.lowercase()
+C) lower()
+D) toLower()
+Answer: C
+
+23. What will print([1, 2, 3] + [4, 5]) output?
+A) [1, 2, 3, 4, 5]
+B) 12345
+C) [1, 2, 3][4, 5]
+D) Error
+Answer: A
+
+24. How do you add an element 10 to the end of a list my_list?
+A) my_list.add(10)
+B) my_list.append(10)
+C) my_list.insert(10)
+D) my_list.extend(10)
+Answer: B
+
+25. What is the output of len("Python Programming")?
+A) 17
+B) 18
+C) 19
+D) 20
+Answer: A
+
+
+26. How do you access the value associated with key 'name' in my_dict = {'name': 'John'}?
+A) my_dict.name
+B) my_dict['name']
+C) my_dict(name)
+D) my_dict->name
+Answer: B
+
+27. What will my_tuple = (1, 2, 3) return for my_tuple[1]?
+A) 1
+B) 2
+C) 3
+D) Error
+Answer: B
+
+28. Can dictionary keys be duplicated?
+A) Yes
+B) No
+C) Only if values differ
+D) Only with integers
+Answer: B
+
+29. How do you remove a key-value pair from a dictionary?
+A) delete my_dict[key]
+B) my_dict.remove(key)
+C) del my_dict[key]
+D) my_dict.popitem()
+Answer: C
+
+30. What will print((1, 2) + (3, 4)) output?
+A) (1, 2, 3, 4)
+B) 1234
+C) (1, 2)(3, 4)
+D) Error
+Answer: A
+
+
+31. What mode is used to read a file in Python?
+A) w
+B) a
+C) r
+D) x
+Answer: C
+
+32. How do you close an open file f?
+A) f.stop()
+B) f.close()
+C) f.exit()
+D) f.quit()
+Answer: B
+
+33. What will f = open("test.txt", "w") do if test.txt already exists?
+A) Append to the file
+B) Raise an error
+C) Overwrite the file
+D) Open in read mode
+Answer: C
+
+34. Which method reads the entire file as a string?
+A) readline()
+B) read()
+C) readlines()
+D) fetch()
+Answer: B
+
+35. What happens if you try to read a closed file?
+A) None
+B) ''
+C) Error
+D) 0
+Answer: C
+
+
+36. Which keyword is used for exception handling?
+A) error
+B) catch
+C) except
+D) finally
+Answer: C
+
+37. What will int("abc") raise?
+A) TypeError
+B) ValueError
+C) SyntaxError
+D) None
+Answer: B
+
+38. How do you raise a custom exception?
+A) throw Exception("Error")
+B) raise Exception("Error")
+C) error("Error")
+D) except Exception("Error")
+Answer: B
+
+39. Which operator checks for object identity?
+A) ==
+B) !=
+C) is
+D) in
+Answer: C
+
+40. What is the output of print(10 > 5 and 3 < 1)?
+A) True
+B) False
+C) None
+D) Error
+Answer: B
+
+
+41. What is recursion?
+A) A function calling itself
+B) Looping through a list
+C) Importing a module
+D) Declaring variables
+Answer: A
+
+42. Which method is used to sort a list in place?
+A) sort()
+B) sorted()
+C) order()
+D) reverse()
+Answer: A
+
+43. What is the output of print(type([1, 2, 3]))?
+A) tuple
+B) list
+C) set
+D) dict
+Answer: B
+
+44. Which of the following creates an empty dictionary?
+A) {}
+B) []
+C) dict()
+D) set()
+Answer: C
+
+45. What will print(5 not in [1, 2, 3]) output?
+A) True
+B) False
+C) None
+D) Error
+Answer: A
+
+
+46. What does pass do in Python?
+A) Skip execution
+B) End the program
+C) Raise an error
+D) Define a function
+Answer: A
+
+47. What is the output of print(str(100))?
+A) 100
+B) "100"
+C) 1 0 0
+D) Error
+Answer: B
+
+48. Which loop will execute at least once?
+A) for
+B) while
+C) do-while
+D) None
+Answer: D (Python has no do-while)
+
+49. What will print(10 % 3) output?
+A) 3
+B) 1
+C) 0
+D) 10
+Answer: B
+
+50. How do you check the length of a list?
+A) length()
+B) count()
+C) len()
+D) size()
+Answer: C  
