c3492003e62dae8e7b57e2e244f20e07dec71380
Aadesh <aadeshlokhande11@gmail.com>
2025-03-05 01:13:50 +0530

    none

---
+++ EnglishPractice/fakerData.py
@@ -0,0 +1,17 @@
+from faker import Faker
+fake = Faker()
+
+a = fake.name()
+# 'Lucy Cechtelar'
+
+# fake.address()
+# '426 Jordy Lodge
+#  Cartwrightshire, SC 88120-6700'
+
+# fake.text()
+
+print(a)
+
+
+
+
